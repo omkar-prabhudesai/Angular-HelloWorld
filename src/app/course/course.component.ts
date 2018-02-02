1ed1d5cb3fd5050e99f640364850ec5745f4639f
omkar.prabhudesai@gmail.com <omkar.prabhudesai@gmail.com>
2018-02-02 17:17:03 +0530

    test working

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -7,7 +7,7 @@ import { CoursesService } from '../courses.service';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-
+  // This is comment
   title = "List of Courses";
   imageUrl = "http://lorempixel.com/400/200";
   colSpan = 2;
